c2c10ec6ae98c4b902215da99651b2befbe26075
ArtyomMinsk <artem.nesterenko@gmail.com>
2016-07-13 23:39:47 -0400

    Submitting Normal Mode assignment

--- palindrome.py
+++ palindrome.py
@@ -1,12 +1,29 @@
-def is_palindrome(sentence):
-    # TODO: return True or False if the sentence is or isn't a palindrome
-    pass
+import re
 
+my_reversed_list = []
+
+def is_polindrome(sentence):
+    if sentence == []:
+        return True
+
+    is_polindrome(sentence[1:])
+    my_reversed_list.append(sentence[0])
 
 def main():
-    # TODO: put your input/output code here
-    pass
+    sentence = input("Please input a word/sentence: ").lower()
+    characters_only = re.sub("[^A-Za-z0-9]", "", sentence) #removes special characters
+    print("Your sentence without the special characters is: ", characters_only)
+    sentence = list(characters_only)
 
+    is_polindrome(sentence)
+
+    print(sentence)
+    print(my_reversed_list)
+
+    if sentence == my_reversed_list:
+        print("Your sentence is a palindrome!")
+    else:
+        print("Your sentence is not a palindrome!")
 
 if __name__ == '__main__':
     main()
