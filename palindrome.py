1297ec6573b9883581f4d59c54f43ca7f82c3a53
ArtyomMinsk <artem.nesterenko@gmail.com>
2016-07-14 00:31:19 -0400

    Restructured code to work with test file

--- palindrome.py
+++ palindrome.py
@@ -3,27 +3,33 @@ import re
 my_reversed_list = []
 
 def is_palindrome(sentence):
-    if sentence == []:
-        return True
-
-    is_palindrome(sentence[1:])
-    my_reversed_list.append(sentence[0])
-
-def main():
-    sentence = input("Please input a word/sentence: ").lower()
-    characters_only = re.sub("[^A-Za-z0-9]", "", sentence) #removes special characters
+    characters_only = re.sub("[^A-Za-z0-9]", "", sentence).lower() #removes special characters
     print("Your sentence without the special characters is: ", characters_only)
+
     sentence = list(characters_only)
-
-    is_palindrome(sentence)
-
     print(sentence)
+
+    reverse_list(sentence)
     print(my_reversed_list)
 
     if sentence == my_reversed_list:
         print("Your sentence is a palindrome!")
+        return True
     else:
         print("Your sentence is not a palindrome!")
+        return False
+
+def reverse_list(sentence):
+    if sentence == []:
+        return True
+
+    reverse_list(sentence[1:])
+    my_reversed_list.append(sentence[0])
+
+def main():
+    sentence = input("Please input a word/sentence: ")
+
+    is_palindrome(sentence)
 
 if __name__ == '__main__':
     main()
