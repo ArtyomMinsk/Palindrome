e753836d0a06a6ce763e3e35e700fe0c7abf06bf
ArtyomMinsk <artem.nesterenko@gmail.com>
2016-07-13 23:41:11 -0400

    Submitting the Advanced Mode assignment

---
+++ hw3_advanced.py
@@ -0,0 +1,30 @@
+import re
+
+my_reversed_list = []
+
+def is_polindrome(sentence):
+    length = len(sentence)
+
+    for item in sentence:
+        my_reversed_list.append(sentence[length-1])
+        length -= 1
+
+    print(sentence)
+    print(my_reversed_list)
+
+    if sentence == my_reversed_list:
+        print("Your sentence is a palindrome")
+    else:
+        print("Your sentence is not a palindrome")
+
+
+def main():
+    sentence = input("Please input a word/sentence: ").lower()
+    characters_only = re.sub("[^A-Za-z0-9]", "", sentence)
+    print("Your sentence without the special characters is: ", characters_only)
+    sentence = list(characters_only)
+
+    is_polindrome(sentence)
+
+if __name__ == '__main__':
+    main()
